5cb416f97deff6539bdfa59257edf11d86d989cf
Adrian Woźniak <adrianwozniak20@gmail.com>
2025-03-25 19:03:45 +0100

    products list prepared

---
+++ products.js
@@ -0,0 +1,44 @@
+{
+    const products = [  
+        {
+            category: "fruits/vegetables",
+            name: "frozen strawberries",
+            kcal: 35,
+            whey: 1,
+            fat: 0,
+            carbs: 8,
+        },
+        {
+            category: "fruits/vegetables",
+            name: "banana",
+            kcal: 89,
+            whey: 1,
+            fat: 0,
+            carbs: 23,
+        },
+        {
+            category: "fruits/vegetables",
+            name: "apple",
+            kcal: 57,
+            whey: 0,
+            fat: 0,
+            carbs: 14,
+        },
+        {
+            category: "whey",
+            name: "lean cottage cheese",
+            kcal: 83,
+            whey: 17,
+            fat: 0,
+            carbs: 4,
+        },
+        {
+            category: "carbs",
+            name: "oat flakes",
+            kcal: 354,
+            whey: 12,
+            fat: 6,
+            carbs: 56,
+        },
+    ];
+}
