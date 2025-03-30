164e4048d27037023e1ef63141ba3bfed0e34558
Adrian Woźniak <adrianwozniak20@gmail.com>
2025-03-30 14:29:12 +0200

    files structure changed

---
+++ JS/productWeightCounter.js
@@ -0,0 +1,29 @@
+const productWeightSelector = document.querySelector(".js-productWeight");
+let productWeight = Number(productWeightSelector.innerText);
+const addWeight = document.querySelectorAll(".js-plusButton");
+const substractWeight = document.querySelectorAll(".js-minusButton");
+const productForm = document.querySelectorAll(".js-form");
+
+addWeight.forEach((button) =>
+    button.addEventListener("click", () => {
+        productWeight += 10;
+        productWeightSelector.innerText = productWeight;
+    }
+    )
+);
+
+substractWeight.forEach((button) =>
+    button.addEventListener("click", () => {
+        if (productWeight <= 0) {
+            return;
+        }
+
+        productWeight -= 10;
+        productWeightSelector.innerText = productWeight;
+    })
+);
+
+
+productForm.forEach((form) => {
+    form.addEventListener("submit", (event) => event.preventDefault());
+});
