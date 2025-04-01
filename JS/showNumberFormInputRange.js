297699ae78a90d511cc74252fd72b4fb83ccbcd5
Adrian Woźniak <adrianwozniak20@gmail.com>
2025-04-01 19:09:49 +0200

    input range default value adjusted

--- JS/showNumberFormInputRange.js
+++ JS/showNumberFormInputRange.js
@@ -1,7 +1,6 @@
 let inputRangeValue = document.querySelector(".js-productsNumberRange");
 const numberFromInputRange = document.querySelector(".js-showNumberFromInputRange");
 
-
 inputRangeValue.addEventListener("change", (event) => (
     numberFromInputRange.innerText = event.target.value
 ));
