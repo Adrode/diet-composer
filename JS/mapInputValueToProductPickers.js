f329d3aa9c1493721eb0993875095c2a87fe6638
Adrian Woźniak <adrianwozniak20@gmail.com>
2025-04-02 19:30:44 +0200

    productPickers mapping finished

--- JS/mapInputValueToProductPickers.js
+++ JS/mapInputValueToProductPickers.js
@@ -1,32 +1,28 @@
 let inputRangeValue = document.querySelector(".js-productsNumberRange");
 const numberFromInputRange = document.querySelector(".js-showNumberFromInputRange");
-const productPickers = document.querySelector(".js-productPickers");
+let productPickers = document.querySelector(".js-productPickers");
+
+const generateProductPickers = () => {
+    return Array.from({ length: Number(inputRangeValue.value) }).map((_, index) => `
+        <div class="product">
+            <div class="product__weight"><span class="js-productWeight${index + 1}">100</span>g</div>
+            <form class="product__options js-form${index + 1}">
+                <button class="product__sign js-minusButton${index + 1}">-</button>
+                <select class="js-productSelect${index + 1}"></select>
+                <button class="product__sign js-plusButton${index + 1}">+</button>
+            </form>
+            <div class="product__macros">
+                <div><span class="js-productKcal${index + 1}">[]</span>kcal</div>
+                <div><span class="js-productWhey${index + 1}">[]</span>whey</div>
+                <div><span class="js-productFat${index + 1}">[]</span>fat</div>
+                <div><span class="js-productCarbs${index + 1}">[]</span>carbs</div>
+            </div>
+        </div>`
+    ).join("")
+};
 
 inputRangeValue.addEventListener("input", (event) => {
-    console.log(Array.from({ length: inputRangeValue.value }), (_, index) => index + 1);
-    
     numberFromInputRange.innerText = event.target.value;
+    productPickers.innerHTML = generateProductPickers();
+    console.log(productPickers.innerHTML);
 });
-
-
-
-
-
-
-
-/*
-            <div class="product">
-                <div class="product__weight"><span class="js-productWeight">100</span>g</div>
-                <form class="product__options js-form">
-                    <button class="product__sign js-minusButton">-</button>
-                    <select class="js-productSelect"></select>
-                    <button class="product__sign js-plusButton">+</button>
-                </form>
-                <div class="product__macros">
-                    <div><span class="js-productKcal">[]</span>kcal</div>
-                    <div><span class="js-productWhey">[]</span>whey</div>
-                    <div><span class="js-productFat">[]</span>fat</div>
-                    <div><span class="js-productCarbs">[]</span>carbs</div>
-                </div>
-            </div>
-*/
