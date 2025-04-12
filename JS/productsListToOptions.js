da67267951a34751001967012e9e6d8ba693c74c
Adrian Woźniak <adrianwozniak20@gmail.com>
2025-04-12 17:26:45 +0200

    removed unnecessary JS files

--- JS/productsListToOptions.js
+++
@@ -1,8 +0,0 @@
-import { products } from "./products.js";
-
-const productsToOptions = products.map((product) => `
-    <option>${product.name}</option>
-`);
-
-const productSelect = document.querySelectorAll(".js-productSelect");
-productSelect.forEach(product => product.innerHTML = productsToOptions); 
